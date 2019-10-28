52b29b21ca4fbecd1597865150118179c5d1b2bd
Jorge Escobar <jorge.escobar@conexiogroup.com>
2019-10-28 14:33:28 -0600

    Reformat Code

--- src/com/agileengine/jescobar/Utils.java
+++ src/com/agileengine/jescobar/Utils.java
@@ -13,53 +13,55 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
- final class Utils {
-    private Utils (){}
+final class Utils {
+    private Utils() {
+    }
 
-     static Element findElementById(File htmlFile, String targetElementId) throws IOException {
+    static Element findElementById(File htmlFile, String targetElementId) throws IOException {
         Document doc = getDocument(htmlFile);
         return doc.getElementById(targetElementId);
 
     }
-     static String stringifiedAttributesElement(Element element) {
+
+    static String stringifiedAttributesElement(Element element) {
         return element.attributes().asList().stream()
                 .map(attr -> attr.getKey() + " = " + attr.getValue())
                 .collect(Collectors.joining(", "));
     }
 
-     static String getElementPath(Element element) {
+    static String getElementPath(Element element) {
         StringBuilder stringBuilder = new StringBuilder();
 
-         for (int i = element.parents().size() - 1; i >= 0; i--) {
-             Element parent = element.parents().get(i);
-             stringBuilder.append(parent.tag()).append(">");
-         }
-         stringBuilder.append(element);
+        for (int i = element.parents().size() - 1; i >= 0; i--) {
+            Element parent = element.parents().get(i);
+            stringBuilder.append(parent.tag()).append(">");
+        }
+        stringBuilder.append(element);
         return stringBuilder.toString();
     }
 
-     static List<Element> getElementsByTag(File html, Tag tag) throws IOException{
+    static List<Element> getElementsByTag(File html, Tag tag) throws IOException {
         Document document = getDocument(html);
         return document.getElementsByTag(tag.getName());
     }
 
-    private static Document getDocument(File htmlFille) throws IOException {
-        return Jsoup.parse(htmlFille, StandardCharsets.UTF_8.name(), htmlFille.getAbsolutePath());
+    private static Document getDocument(File htmlFile) throws IOException {
+        return Jsoup.parse(htmlFile, StandardCharsets.UTF_8.name(), htmlFile.getAbsolutePath());
     }
 
 
-     static Element getSimilarElement(String source, List<Element> elementsByTag) {
+    static Element getSimilarElement(String source, List<Element> elementsByTag) {
         NormalizedStringDistance detector = new NormalizedLevenshtein();
 
-         double acceptableDistance = 1.0;
+        double acceptableDistance = 1.0;
         Element similarElement = null;
-         for (Element element : elementsByTag) {
-             double compareDistance = detector.distance(source, stringifiedAttributesElement(element));
-             if (compareDistance < acceptableDistance) {
-                 acceptableDistance = compareDistance;
-                 similarElement = element;
-             }
-         }
+        for (Element element : elementsByTag) {
+            double compareDistance = detector.distance(source, stringifiedAttributesElement(element));
+            if (compareDistance < acceptableDistance) {
+                acceptableDistance = compareDistance;
+                similarElement = element;
+            }
+        }
         return similarElement;
 
     }
